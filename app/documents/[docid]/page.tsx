55eb004115925edc9582342eca86a4500c5109ba
AkramRamoul <ahmedakramramoul@gmail.com>
2024-12-17 17:34:37 +0100

    tiptap setup

---
+++ app/documents/[docid]/page.tsx
@@ -0,0 +1,11 @@
+import Editor from "./editor";
+
+async function Home() {
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  );
+}
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 function Home() {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="flex items-center justify-center min-h-screen">
+      <Link href="/documents/123">
+        Click <span className="underline text-blue-500">Here</span> to go
+        dynamuc route
+      </Link>
     </div>
   );
 }
