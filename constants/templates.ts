8a40eb4cfa491c8b39d3aeb702a39c2759571337
AkramRamoul <ahmedakramramoul@gmail.com>
2025-01-01 21:04:27 +0100

    template page

---
+++ constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/blank-document.svg",
+  },
+  {
+    id: "buisness-letter",
+    label: "Buisness Document",
+    imageUrl: "/business-letter.svg",
+  },
+  {
+    id: "software-propposal",
+    label: "Software Develpoemnet Proposal",
+    imageUrl: "/software-proposal.svg",
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/resume.svg",
+  },
+  {
+    id: "cover-letter",
+    label: "Cover Letter",
+    imageUrl: "/cover-letter.svg",
+  },
+  {
+    id: "letter",
+    label: "Letter",
+    imageUrl: "/letter.svg",
+  },
+  {
+    id: "proposal",
+    label: "Proposal",
+    imageUrl: "/project-proposal.svg",
+  },
+];
