427142486558a24864c69802cb2b537934550b4a
AkramRamoul <ahmedakramramoul@gmail.com>
2025-01-02 21:06:27 +0100

    reading content

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useQuery } from "convex/react";
 import NavBar from "./navbar";
 import Templates from "./template-gallery";
+import { api } from "@/convex/_generated/api";
+import { Loader2 } from "lucide-react";
 
 function Home() {
+  const documents = useQuery(api.documents.listDocuments);
+  if (documents === undefined)
+    return (
+      <div className="flex items-center justify-center w-full h-screen">
+        <Loader2 className="animate-spin h-12 w-12" />
+      </div>
+    );
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +21,7 @@ function Home() {
       </div>
       <div className="mt-16">
         <Templates />
+        {documents?.map((document) => <div key={document._id} />)}
       </div>
     </div>
   );
