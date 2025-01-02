5e3d8f2e91c067f7da5c31bd06a78912ad109d75
AkramRamoul <ahmedakramramoul@gmail.com>
2025-01-02 22:06:45 +0100

    clerk auth setup

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -1,10 +1,35 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {
+  ConvexReactClient,
+  Authenticated,
+  Unauthenticated,
+  AuthLoading,
+} from "convex/react";
 import { ReactNode } from "react";
-
+import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { Loader2 } from "lucide-react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <Unauthenticated>
+          <div className="flex items-center justify-center w-full h-screen">
+            <SignIn />
+          </div>
+        </Unauthenticated>
+        <AuthLoading>
+          <div className="flex items-center justify-center w-full h-screen">
+            <Loader2 className="animate-spin h-12 w-12" />
+          </div>
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://cosmic-bengal-99.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
