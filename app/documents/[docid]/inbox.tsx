020b4028864eb092a9ce70cfa38f10d08002e6d6
AkramRamoul <ahmedakramramoul@gmail.com>
2025-01-07 13:57:01 +0100

    Notification icon bug fix

--- app/documents/[docid]/inbox.tsx
+++ app/documents/[docid]/inbox.tsx
@@ -12,7 +12,16 @@ import {
 } from "@/components/ui/dropdown-menu";
 function Inbox() {
   return (
-    <ClientSideSuspense fallback={null}>
+    <ClientSideSuspense
+      fallback={
+        <>
+          <Button disabled variant={"ghost"} className="relative" size={"icon"}>
+            <BellIcon className="size-5" />
+          </Button>
+          <Separator orientation="vertical" className="h-6" />
+        </>
+      }
+    >
       <InboxMenu />
     </ClientSideSuspense>
   );
@@ -22,37 +31,41 @@ export default Inbox;
 
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 function InboxMenu() {
   const { inboxNotifications } = useInboxNotifications();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={"ghost"} className="relative" size={"icon"}>
-          <BellIcon className="size-5" />
-          {inboxNotifications.length > 0 && (
-            <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
-              {inboxNotifications.length}
-            </span>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} className="relative" size={"icon"}>
+            <BellIcon className="size-5" />
+            {inboxNotifications.length > 0 && (
+              <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
+                {inboxNotifications.length}
+              </span>
+            )}
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-auto">
+          {inboxNotifications.length > 0 ? (
+            <InboxNotificationList>
+              {inboxNotifications.map((inboxNotification) => (
+                <InboxNotification
+                  key={inboxNotification.id}
+                  inboxNotification={inboxNotification}
+                />
+              ))}
+            </InboxNotificationList>
+          ) : (
+            <div className="p-2 w-[400px] text-sm text-muted-foreground ">
+              No notification
+            </div>
           )}
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-auto">
-        {inboxNotifications.length > 0 ? (
-          <InboxNotificationList>
-            {inboxNotifications.map((inboxNotification) => (
-              <InboxNotification
-                key={inboxNotification.id}
-                inboxNotification={inboxNotification}
-              />
-            ))}
-          </InboxNotificationList>
-        ) : (
-          <div className="p-2 w-[400px] text-sm text-muted-foreground ">
-            No notification
-          </div>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Separator orientation="vertical" className="h-6" />
+    </>
   );
 }
